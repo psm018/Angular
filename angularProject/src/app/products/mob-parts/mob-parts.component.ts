6a0d58107149df2663474919c99854f1660e4992
prashantmalegaonkar <prashant.m@finiq.com>
2020-02-14 10:37:10 +0530

    a

--- angularProject/src/app/products/mob-parts/mob-parts.component.ts
+++ angularProject/src/app/products/mob-parts/mob-parts.component.ts
@@ -17,7 +17,27 @@ import { MobParts } from "./mob-parts";
 export class MobPartsComponent {
   // title:string = "some text goes here...!"
 
-  mobParts: MobParts[] = MOBPARTS;
+  mobParts: MobParts[];
+
+  ngOnInit() {
+    console.log("ngOnInit Block...!");
+    this.mobParts = MOBPARTS;
+  }
+  constructor() {
+    console.log("We are in 'constructor...!");
+  }
+  ngOnDestroy() {
+    console.log("ngOnDestroy Block...!");
+    this.mobParts = [];
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit Block...!");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit Block...!");
+  }
 
   calcProd() {
     let sum = 0;
